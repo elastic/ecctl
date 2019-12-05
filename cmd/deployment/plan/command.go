88c79383e16b99db1e24264c0e5ac9b163074bd8
Marc Lopez Rubio <marc5.12@outlook.com>
2019-12-05 08:48:15 +0100

    cmd: Add deployment plan cancel (#72)
    Adds the deployment plan cancel command which allows users to cancel
    pending plans for specific resources.
    The syntax is as follows:
    ecctl deployment plan cancel <deployment id> --type <type> --ref-id <id>
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- cmd/deployment/plan/command.go
+++ cmd/deployment/plan/command.go
@@ -15,34 +15,18 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package cmddeployment
+package cmddeploymentplan
 
 import (
 	"github.com/spf13/cobra"
-
-	cmdapm "github.com/elastic/ecctl/cmd/deployment/apm"
-	cmdelasticsearch "github.com/elastic/ecctl/cmd/deployment/elasticsearch"
-	cmdkibana "github.com/elastic/ecctl/cmd/deployment/kibana"
-	cmddeploymentnote "github.com/elastic/ecctl/cmd/deployment/note"
-	cmddeploymentplan "github.com/elastic/ecctl/cmd/deployment/plan"
 )
 
 // Command is the deployment subcommand
 var Command = &cobra.Command{
-	Use:     "deployment",
-	Short:   "Manages deployments",
+	Use:     "plan",
+	Short:   "Manages deployment plans",
 	PreRunE: cobra.MaximumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
-}
-
-func init() {
-	Command.AddCommand(
-		cmddeploymentnote.Command,
-		cmdelasticsearch.Command,
-		cmdkibana.Command,
-		cmdapm.Command,
-		cmddeploymentplan.Command,
-	)
 }
