f3709f2068ac52bfc58c01258abf75a4dd002622
Marc Lopez <marc5.12@outlook.com>
2019-12-09 12:00:31 +0100

    cmd: Add deployment resource upgrade command
    Adds a command which allows upgrading a stateless resource as specified
    in the help and long description for commands. The supported type is any
    of the stateless resource types, which for now are apm, appsearch and
    kibana.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ docs/ecctl_deployment_resource_upgrade.md
@@ -0,0 +1,46 @@
+## ecctl deployment resource upgrade
+
+Upgrades a deploymnt resource
+
+### Synopsis
+
+Upgrades a stateless deployment resource so it matches the Elasticsearch
+deployment version. Only stateless resources are supported in the --type flag
+
+```
+ecctl deployment resource upgrade <deployment id> --type <type> --ref-id <ref-id> [flags]
+```
+
+### Options
+
+```
+  -h, --help            help for upgrade
+      --ref-id string   Optional deployment type RefId, if not set, the RefId will be auto-discovered
+  -t, --track           Tracks the progress of the performed task
+      --type string     Optional deployment type to show resource information (elasticsearch, kibana, apm, or appsearch)
+```
+
+### Options inherited from parent commands
+
+```
+      --apikey string      API key to use to authenticate (If empty will look for EC_APIKEY environment variable)
+      --config string      Config name, used to have multiple configs in $HOME/.ecctl/<env> (default "config")
+      --force              Do not ask for confirmation
+      --format string      Formats the output using a Go template
+      --host string        Base URL to use
+      --insecure           Skips all TLS validation
+      --message string     A message to set on cluster operation
+      --output string      Output format [text|json] (default "text")
+      --pass string        Password to use to authenticate (If empty will look for EC_PASS environment variable)
+      --pprof              Enables pprofing and saves the profile to pprof-20060102150405
+  -q, --quiet              Suppresses the configuration file used for the run, if any
+      --timeout duration   Timeout to use on all HTTP calls (default 30s)
+      --trace              Enables tracing saves the trace to trace-20060102150405
+      --user string        Username to use to authenticate (If empty will look for EC_USER environment variable)
+      --verbose            Enable verbose mode
+```
+
+### SEE ALSO
+
+* [ecctl deployment resource](ecctl_deployment_resource.md)	 - Manages deployment resources
+
---
+++ pkg/deployment/depresource/upgrade_stateless.go
@@ -0,0 +1,79 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package depresource
+
+import (
+	"errors"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/client/deployments"
+	"github.com/elastic/cloud-sdk-go/pkg/models"
+	"github.com/hashicorp/go-multierror"
+
+	"github.com/elastic/ecctl/pkg/deployment/deputil"
+	"github.com/elastic/ecctl/pkg/util"
+)
+
+// UpgradeStatelessParams is consumed by UpgradeStateless
+type UpgradeStatelessParams struct {
+	*api.API
+
+	DeploymentID string
+	Type         string
+	RefID        string
+}
+
+// Validate ensures the parameters are usable by the consuming function.
+func (params UpgradeStatelessParams) Validate() error {
+	var merr = new(multierror.Error)
+
+	if params.API == nil {
+		merr = multierror.Append(merr, util.ErrAPIReq)
+	}
+
+	if len(params.DeploymentID) != 32 {
+		merr = multierror.Append(merr, deputil.NewInvalidDeploymentIDError(params.DeploymentID))
+	}
+
+	if params.Type == "" {
+		merr = multierror.Append(merr, errors.New("deployment resource type cannot be empty"))
+	}
+
+	return merr.ErrorOrNil()
+}
+
+// UpgradeStateless upgrades a stateless deployment resource like APM, Kibana
+// and AppSearch.
+func UpgradeStateless(params UpgradeStatelessParams) (*models.DeploymentResourceUpgradeResponse, error) {
+	if err := params.Validate(); err != nil {
+		return nil, err
+	}
+
+	res, err := params.V1API.Deployments.UpgradeDeploymentStatelessResource(
+		deployments.NewUpgradeDeploymentStatelessResourceParams().
+			WithStatelessResourceKind(params.Type).
+			WithDeploymentID(params.DeploymentID).
+			WithRefID(params.RefID),
+		params.AuthWriter,
+	)
+	if err != nil {
+		return nil, api.UnwrapError(err)
+	}
+
+	return res.Payload, nil
+}
