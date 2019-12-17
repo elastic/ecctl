7c2be0412cedb66af34a32b29b345fad24b21712
Marc Lopez Rubio <marc5.12@outlook.com>
2019-12-17 08:37:56 +0100

    cmd: Add deployment resource delete command (#88)
    Adds an `ecctl deployment resource delete` command which deletes a
    previously shut down deployment resource. If ref-id not specified
    it's auto-discovered via an API call.
    
    Required flags: `--type`
    Optional flags: `--ref-id`
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ pkg/deployment/depresource/delete_stateless.go
@@ -0,0 +1,64 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package depresource
+
+import (
+	"errors"
+
+	"github.com/elastic/cloud-sdk-go/pkg/client/deployments"
+	"github.com/hashicorp/go-multierror"
+
+	"github.com/elastic/ecctl/pkg/deployment"
+	"github.com/elastic/ecctl/pkg/util"
+)
+
+// DeleteStatelessParams is consumed by Delete
+type DeleteStatelessParams struct {
+	deployment.ResourceParams
+}
+
+// Validate ensures the parameters are usable by the consuming function.
+func (params DeleteStatelessParams) Validate() error {
+	var merr = new(multierror.Error)
+
+	merr = multierror.Append(merr, params.ResourceParams.Validate())
+
+	if params.Type == "elasticsearch" {
+		merr = multierror.Append(merr, errors.New("deployment resource type \"elasticsearch\" is not supported"))
+	}
+
+	return merr.ErrorOrNil()
+}
+
+// DeleteStateless upgrades a stateless deployment resource like APM, Kibana
+// and AppSearch.
+func DeleteStateless(params DeleteStatelessParams) error {
+	if err := params.Validate(); err != nil {
+		return err
+	}
+
+	return util.ReturnErrOnly(
+		params.V1API.Deployments.DeleteDeploymentStatelessResource(
+			deployments.NewDeleteDeploymentStatelessResourceParams().
+				WithStatelessResourceKind(params.Type).
+				WithDeploymentID(params.DeploymentID).
+				WithRefID(params.RefID),
+			params.AuthWriter,
+		),
+	)
+}
