30757f100813a21974876c7c6a4d3414688383d8
Marc Lopez <marc5.12@outlook.com>
2020-01-13 07:47:41 +0900

    release: Automate release in GitHub action
    Automates the release process for ecctl binaries in a GitHub action. As
    part of automating the release, it has been split into two parts;
    running goreleaser and running goreleaser post actions.
    
    Since goreleaser no longer automates the homebrew formula creation or
    the upload of binaries to an artifact repository, those actions have
    been moved to `scripts/goreleaser-post-actions.sh`, which uploads the
    binaries to S3, Creates GitHub release, and updates elastic/homebrew-tap
    with the latest ecctl version, binary links and checksums.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ scripts/goreleaser-post-actions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Parameters
+VERSION=${1}
+
+# Upload the binaries and the checksums.
+for f in dist/*.{tar.gz,deb,rpm,txt}; do
+    aws --profile ecsecurity s3 cp --acl bucket-owner-full-control \
+    ${f} s3://download.elasticsearch.org/downloads/ecctl/${VERSION}/
+done
+
+# Create the actual Github Release.
+hub release create -F notes/${VERSION}.md -m ${VERSION}
+
+# Update the brew tap formula
+./scripts/update-brew-tap.sh ${VERSION}
---
+++ scripts/update-brew-tap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Parameters
+VERSION=$(echo ${1}| sed 's/^v//')
+
+# Globals
+FORMULA_FILE=/tmp/homebrew-tap/Formula/ecctl.rb
+GITHUB_USER=marclop
+
+# Execution
+
+if [[ -d /tmp/homebrew-tap ]]; then rm -rf /tmp/homebrew-tap; fi
+hub clone https://github.com/elastic/homebrew-tap /tmp/homebrew-tap
+
+DARWIN_CHECKSUM=$(grep darwin_amd64 dist/*checksums.txt | awk '{print $1}' | tr -d '\n')
+LINUX_CHECKSUM=$(grep linux_amd64.tar.gz dist/*checksums.txt | awk '{print $1}' | tr -d '\n')
+OLD_DARWIN_CHECKSUM=$(grep sha256 ${FORMULA_FILE}|head -1| awk '{print $2}' | tr -d '\n' | tr -d '"')
+OLD_LINUX_CHECKSUM=$(grep sha256 ${FORMULA_FILE}|tail -1| awk '{print $2}' | tr -d '\n' | tr -d '"')
+OLD_VERSION=$(grep 'version \"' ${FORMULA_FILE} | awk '{print $2}' | tr -d '"' | tr -d '\n')
+
+sed "s/${OLD_VERSION}/${VERSION}/g" ${FORMULA_FILE} | sed "s/${OLD_DARWIN_CHECKSUM}/${DARWIN_CHECKSUM}/" | sed "s/${OLD_LINUX_CHECKSUM}/${LINUX_CHECKSUM}/" > /tmp/ecctl.rb
+mv /tmp/ecctl.rb ${FORMULA_FILE}
+
+cd /tmp/homebrew-tap
+hub fork --no-remote
+hub remote add fork https://github.com/${GITHUB_USER}/homebrew-tap
+hub add Formula/ecctl.rb
+hub checkout -b f/update-ecctl-formula-to-${VERSION}
+hub commit -m "(TEST) Update ecctl version to ${VERSION}"
+hub push fork f/update-ecctl-formula-to-${VERSION}
+hub pull-request -m "Update ecctl version to ${VERSION}" -m "Created through automation by update-brew-tap.sh"
