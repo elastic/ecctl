b7ae0d6eef4b6d144bbe24ca869ed38355ce047b
Karen Cárcamo <karencfv@users.noreply.github.com>
2020-02-20 21:44:54 +1300

    cmd: new runner search (#192)
    Implements a new `ecctl runner search` command with two optional --query and --file flags.
    
    Also a tiny refactor to the allocator search command to include two new flag helper functions.

---
+++ pkg/platform/runner/search.go
@@ -0,0 +1,60 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package runner
+
+import (
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/client/platform_infrastructure"
+	"github.com/elastic/cloud-sdk-go/pkg/models"
+	"github.com/go-openapi/strfmt"
+	"github.com/hashicorp/go-multierror"
+)
+
+// SearchParams contains parameters used to search runner's data using Query DSL
+type SearchParams struct {
+	Params
+	Request models.SearchRequest
+}
+
+// Validate is the implementation for the ecctl.Validator interface
+func (params SearchParams) Validate() error {
+	var merr = new(multierror.Error)
+
+	merr = multierror.Append(merr, params.Params.Validate())
+
+	merr = multierror.Append(merr, params.Request.Validate(strfmt.Default))
+
+	return merr.ErrorOrNil()
+}
+
+// Search searches all the runners using Query DSL
+func Search(params SearchParams) (*models.RunnerOverview, error) {
+	if err := params.Validate(); err != nil {
+		return nil, err
+	}
+
+	res, err := params.API.V1API.PlatformInfrastructure.SearchRunners(
+		platform_infrastructure.NewSearchRunnersParams().
+			WithBody(&params.Request),
+		params.AuthWriter,
+	)
+	if err != nil {
+		return nil, api.UnwrapError(err)
+	}
+	return res.Payload, nil
+}
