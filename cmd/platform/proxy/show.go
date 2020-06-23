77dbbacfb94e0c21b9536ad09f3f3c9be76f6dc6
karencfv <karencfv@gmail.com>
2020-06-23 16:36:09 +1200

    add files

---
+++ cmd/platform/proxy/show.go
@@ -0,0 +1,48 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package cmdproxy
+
+import (
+	"path/filepath"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api/platformapi/proxyapi"
+	"github.com/spf13/cobra"
+
+	"github.com/elastic/ecctl/pkg/ecctl"
+)
+
+var showProxyCmd = &cobra.Command{
+	Use:     "show <proxy id>",
+	Short:   "Returns information about the proxy with given id",
+	PreRunE: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		a, err := proxyapi.Get(proxyapi.GetParams{
+			API:    ecctl.Get().API,
+			Region: ecctl.Get().Config.Region,
+			ID:     args[0],
+		})
+		if err != nil {
+			return err
+		}
+		return ecctl.Get().Formatter.Format(filepath.Join("proxy", "show"), a)
+	},
+}
+
+func init() {
+	Command.AddCommand(showProxyCmd)
+}
