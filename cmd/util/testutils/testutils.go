f300d7f47a8024f9b96e5ba90dd325e2839b6c64
Marc Lopez <marc5.12@outlook.com>
2020-06-29 12:40:42 +0800

    cmd/util/testutils: Add MockApp and TestCommand
    Adds two new functions to the `testutils` package which allow easier
    testing to be written on the `cmd` layer.
    
    All the heavy lifting is done by `testutils.TestCommand` on the test
    function, namely it:
    
        1. Initializes the `ecctl.App` singleton from a `testutils.MockCfg`.
        2. Sets the cobra `cmd.Command` Err and Out to `*Bytes.Buffer`s.
        3. Executes the command with the specified Args.
        4. Asserts the returned errors by `cmd.Execute()`
        5. Asserts the written data to both the `Err` and `Out` buffers.
        6. `ecctl.Cleanup` is called before returning so no instance is set.
    
    It is important to note that global flags won't be parsed and instead,
    must be provided as values to `testutils.MockCfg`. As new global flags
    are added, the structure should be maintained to contain these.
    
    Interaction with the `stdin` hasn't been written yet, but it will be a
    follow-up patch.
    
    Last, the tracker utils have been modified to use `DefaultTestFrequency`
    when the host equals `api.DefaultMockHost` for ease of teasting.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- cmd/util/testutils/testutils.go
+++ cmd/util/testutils/testutils.go
@@ -18,25 +18,78 @@
 package testutils
 
 import (
-	"net/http"
-	"os"
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
 
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/api/mock"
 	"github.com/elastic/cloud-sdk-go/pkg/output"
 
 	"github.com/elastic/ecctl/pkg/ecctl"
-	"github.com/elastic/ecctl/pkg/util"
 )
 
-// MockInitApp initiates a mocked app
-func MockInitApp() error {
-	var config = ecctl.Config{
-		Client:       new(http.Client),
-		OutputDevice: output.NewDevice(os.Stdout),
-		ErrorDevice:  os.Stderr,
-		Output:       "json",
-		Host:         "http://somehost",
-		APIKey:       "helloiamakey",
+const (
+	defaultOutputFormat = "json"
+	defaultAPIKey       = "dummy"
+	defaultRegion       = "ece-region"
+)
+
+// MockCfg represents a small and targeted amount of `ecctl.Config` options
+// aimed at making mocking convenient and easy.
+type MockCfg struct {
+	Responses    []mock.Response
+	Out          io.Writer
+	Err          io.Writer
+	OutputFormat string
+	Format       string
+	Region       string
+
+	Force   bool
+	Verbose bool
+}
+
+func fillDefaults(cfg MockCfg) MockCfg {
+	if cfg.OutputFormat == "" {
+		cfg.OutputFormat = defaultOutputFormat
 	}
 
-	return util.ReturnErrOnly(ecctl.Instance(config))
+	if cfg.Region == "" {
+		cfg.Region = defaultRegion
+	}
+
+	if cfg.Err == nil {
+		cfg.Err = new(bytes.Buffer)
+	}
+
+	if cfg.Out == nil {
+		cfg.Out = new(bytes.Buffer)
+	}
+
+	return cfg
+}
+
+func newConfig(cfg MockCfg) ecctl.Config {
+	cfg = fillDefaults(cfg)
+	return ecctl.Config{
+		Client:       mock.NewClient(cfg.Responses...),
+		OutputDevice: output.NewDevice(cfg.Out),
+		ErrorDevice:  cfg.Err,
+		Output:       cfg.OutputFormat,
+		Format:       cfg.Format,
+		Host:         fmt.Sprintf("https://%s", api.DefaultMockHost),
+		APIKey:       defaultAPIKey,
+		Force:        cfg.Force,
+		Verbose:      cfg.Verbose,
+	}
+}
+
+// MockApp initiates a mocked app from a MockCfg.
+func MockApp(t *testing.T, cfg MockCfg) func() {
+	if _, err := ecctl.Instance(newConfig(cfg)); err != nil {
+		t.Error(err)
+	}
+
+	return ecctl.Cleanup
 }
--- cmd/util/testutils/testutils_test.go
+++ cmd/util/testutils/testutils_test.go
@@ -18,25 +18,78 @@
 package testutils
 
 import (
-	"reflect"
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/api/mock"
+	"github.com/elastic/cloud-sdk-go/pkg/output"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/elastic/ecctl/pkg/ecctl"
 )
 
-func TestMockInitApp(t *testing.T) {
+func Test_newConfig(t *testing.T) {
+	type args struct {
+		cfg MockCfg
+	}
 	tests := []struct {
 		name string
-		err  error
+		args args
+		want ecctl.Config
 	}{
 		{
-			name: "Succeeds mocking initApp",
+			name: "empty params succeed",
+			args: args{cfg: MockCfg{}},
+			want: ecctl.Config{
+				Client:       mock.NewClient(),
+				OutputDevice: output.NewDevice(new(bytes.Buffer)),
+				ErrorDevice:  new(bytes.Buffer),
+				Output:       "json",
+				Host:         fmt.Sprintf("https://%s", api.DefaultMockHost),
+				APIKey:       defaultAPIKey,
+				Force:        false,
+			},
+		},
+		{
+			name: "overriding params succeed",
+			args: args{cfg: MockCfg{
+				Responses: []mock.Response{
+					{Error: errors.New("some error")},
+				},
+				Out:          os.Stdout,
+				Err:          os.Stderr,
+				OutputFormat: "text",
+				Region:       "us-east-1",
+				Force:        true,
+				Verbose:      true,
+			}},
+			want: ecctl.Config{
+				Client:       mock.NewClient(mock.Response{Error: errors.New("some error")}),
+				OutputDevice: output.NewDevice(os.Stdout),
+				ErrorDevice:  os.Stderr,
+				Output:       "text",
+				Host:         fmt.Sprintf("https://%s", api.DefaultMockHost),
+				APIKey:       defaultAPIKey,
+				Force:        true,
+				Verbose:      true,
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := MockInitApp(); !reflect.DeepEqual(err, tt.err) {
-				t.Errorf("MockInitApp() error = %v, wantErr %v", err, tt.err)
-				return
-			}
+			got := newConfig(tt.args.cfg)
+			assert.Equal(t, tt.want, got)
 		})
 	}
+}
+
+func TestMockApp(t *testing.T) {
+	cleanup := MockApp(t, MockCfg{})
+	assert.NotNil(t, ecctl.Get())
+	cleanup()
+	assert.Nil(t, ecctl.Get())
 }
