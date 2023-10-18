92534ee51d9010d53d481b366ae933102b404937
Dominik Giger <dominik.giger@elastic.co>
2023-10-18 16:08:00 +0200

    Update cloud-sdk-go to 1.14.0
    - golangci-lint 1.50.1 doesn't work when go 1.20 is installed -> updated to latest version
    - updated go everywhere to 1.20
    - removed golint, as it is deprecated and unmaintainted. According to the golint site, `go vet` is a replacement
    - with 1.20, `rand.Seed` is deprecated and the random number generator is seeded randomly by default

--- pkg/util/random.go
+++ pkg/util/random.go
@@ -19,14 +19,12 @@ package util
 
 import (
 	"math/rand"
-	"time"
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // RandomString generates a random strings with a defined length.
 func RandomString(n int) string {
-	rand.Seed(time.Now().UnixNano())
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
