f2fc7567c7348b2130c4697889169dea1a37fd16
Dominik Giger <dominik.giger@gmail.com>
2024-09-25 10:34:13 +0200

    Deployment search: Add flag to return all matches. (#664)
    - By default the deployment search only makes one request, returning the number of results specified in 'size'
    - This only works up to 10k results, but may also already stop working before that just due to the size of the response.
    - A new flag now switches to using a cursor to request all matches
    - This works by making a request, then passing the returned cursor into the next request for the next batch of results

--- cmd/deployment/search.go
+++ cmd/deployment/search.go
@@ -18,11 +18,12 @@
 package cmddeployment
 
 import (
+	"fmt"
 	"github.com/elastic/cloud-sdk-go/pkg/api/deploymentapi"
+	"github.com/elastic/cloud-sdk-go/pkg/models"
 	"github.com/elastic/cloud-sdk-go/pkg/util/cmdutil"
-	"github.com/spf13/cobra"
-
 	"github.com/elastic/ecctl/pkg/ecctl"
+	"github.com/spf13/cobra"
 )
 
 const searchQueryLong = `Read more about Query DSL in https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl.html`
@@ -51,16 +52,49 @@ var searchCmd = &cobra.Command{
 			return err
 		}
 
-		res, err := deploymentapi.Search(deploymentapi.SearchParams{
-			API:     ecctl.Get().API,
-			Request: &sr,
-		})
-
-		if err != nil {
-			return err
+		returnAllMatches, _ := cmd.Flags().GetBool("all-matches")
+		if returnAllMatches && sr.Sort == nil {
+			return fmt.Errorf("The query must include a sort-field when using --all-matches. Example: \"sort\": [\"id\"]")
 		}
 
-		return ecctl.Get().Formatter.Format("deployment/search", res)
+		batchSize, _ := cmd.Flags().GetInt32("size")
+
+		var result *models.DeploymentsSearchResponse
+		var cursor string
+		for i := 0; i < 100; i++ {
+			sr.Cursor = cursor
+			if returnAllMatches {
+				// Custom batch-size to override any size already set in the input query
+				sr.Size = batchSize
+			}
+
+			res, err := deploymentapi.Search(deploymentapi.SearchParams{
+				API:     ecctl.Get().API,
+				Request: &sr,
+			})
+
+			if err != nil {
+				return err
+			}
+
+			cursor = res.Cursor
+
+			if result == nil {
+				result = res
+				result.Cursor = "" // Hide cursor in output
+			} else {
+				result.Deployments = append(result.Deployments, res.Deployments...)
+				newReturnCount := *result.ReturnCount + *res.ReturnCount
+				result.ReturnCount = &newReturnCount
+				result.MatchCount = newReturnCount
+			}
+
+			if *res.ReturnCount == 0 || !returnAllMatches {
+				break
+			}
+		}
+
+		return ecctl.Get().Formatter.Format("deployment/search", result)
 	},
 }
 
@@ -68,4 +102,7 @@ func init() {
 	Command.AddCommand(searchCmd)
 	searchCmd.Flags().StringP("file", "f", "", "JSON file that contains JSON-style domain-specific language query")
 	searchCmd.MarkFlagRequired("file")
+	searchCmd.Flags().BoolP("all-matches", "a", false,
+		"Uses a cursor to return all matches of the query (ignoring the size in the query). This can be used to query more than 10k results.")
+	searchCmd.Flags().Int32("size", 500, "Defines the size per request when using the --all-matches option.")
 }
