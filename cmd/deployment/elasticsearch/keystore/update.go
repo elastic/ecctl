a9be0843bb5154ee1f53bd9d0541b1f2465c6c3e
Lisa Cawley <lcawley@elastic.co>
2024-11-26 00:28:41 -0800

    [DOCS] Remove broken link to Cloud API doc (#679)
    (cherry picked from commit 6041fdd1aa909667b88d1748812b157321d94332)

--- cmd/deployment/elasticsearch/keystore/update.go
+++ cmd/deployment/elasticsearch/keystore/update.go
@@ -37,8 +37,7 @@ omitted current keystore items are not removed, unless the secrets are set to "n
 {"secrets": {"my-secret": null}}.
 
 The contents of the specified file should be formatted to match the Elasticsearch Service
-API "KeystoreContents" model. For more information on format, see the ESS API reference:
-https://www.elastic.co/guide/en/cloud/current/definitions.html#KeystoreContents.
+API "KeystoreContents" model.
 `
 
 const updateExample = `# Set credentials for a GCS snapshot repository
